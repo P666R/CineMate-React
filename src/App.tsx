80d8869e08923eab61d2e8aa0fc792443c1e98af
PRoy <sam469roger@gmail.com>
2024-01-25 18:52:31 +0530

    feat-main: changing page title

--- src/App.tsx
+++ src/App.tsx
@@ -14,53 +14,6 @@ interface WatchedMovie extends Movie {
   userRating: number;
 }
 
-const tempMovieData: Movie[] = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt0133093',
-    Title: 'The Matrix',
-    Year: '1999',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt6751668',
-    Title: 'Parasite',
-    Year: '2019',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-  },
-];
-
-const tempWatchedData: WatchedMovie[] = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: 'tt0088763',
-    Title: 'Back to the Future',
-    Year: '1985',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 //! generic function to calculate average
 const average = (arr: number[]): number =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -69,31 +22,12 @@ const KEY: string = '9c76e652';
 
 //! App (structural component) eliminated prop drilling using component composition
 function App(): React.JSX.Element {
-  const [query, setQuery] = useState<string>('inception');
+  const [query, setQuery] = useState<string>('gun');
   const [movies, setMovies] = useState<Movie[]>([] as Movie[]);
   const [watched, setWatched] = useState<WatchedMovie[]>([] as WatchedMovie[]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
   const [selectedId, setSelectedId] = useState<string>(null);
-
-  /*
-  useEffect(function () {
-    console.log('after initial render');
-  }, []);
-
-  useEffect(function () {
-    console.log('after every render');
-  });
-
-  useEffect(
-    function () {
-      console.log('D');
-    },
-    [query]
-  );
-
-  console.log('during render');
- */
 
   function handleSelectMovie(id: string): void {
     setSelectedId((selectedId) => (selectedId === id ? null : id));
@@ -424,6 +358,14 @@ function MovieDetails({
       getMovieDetails();
     },
     [selectedId]
+  );
+
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
   );
 
   return (
